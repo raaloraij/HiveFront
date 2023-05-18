97fb5c6a635aec867587ca9454942d9430dd53a7
raaloraij <116815692+raaloraij@users.noreply.github.com>
2023-05-18 18:18:50 +0300

    Add files via upload

---
+++ hiveFront/hiveFront/hiveFrontApp.swift
@@ -0,0 +1,17 @@
+//
+//  hiveFrontApp.swift
+//  hiveFront
+//
+//  Created by Raghad on 18/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct hiveFrontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
